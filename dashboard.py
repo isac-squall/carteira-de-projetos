9020ea3d3eda13e830801ff0ab92a6fb9bfade39
Seu Nome <seu.email@exemplo.com>
2025-05-14 12:22:51 -0300

    Resolve conflitos após pull

--- dashboard.py
+++ dashboard.py
@@ -13,11 +13,9 @@ def run():
         """
     )
 
-<<<<<<< HEAD
     # Carregar a imagem 'dashpower.png'
     try:
         imagem = Image.open("dashpower.png")
-        # Exibir a imagem no Streamlit
         st.image(imagem, caption="Dashboard Power BI", use_container_width=True)
     except FileNotFoundError:
         st.error("A imagem 'dashpower.png' não foi encontrada. Verifique o caminho e tente novamente.")
@@ -25,14 +23,6 @@ def run():
     # Carregar a imagem 'dashstream.png'
     try:
         imagem = Image.open("dashstream.png")
-        # Exibir a imagem no Streamlit
         st.image(imagem, caption="Dashboard Streamlit", use_container_width=True)
     except FileNotFoundError:
         st.error("A imagem 'dashstream.png' não foi encontrada. Verifique o caminho e tente novamente.")
-
-=======
-    imagem = Image.open("Imagens/dashstream.png")
-    st.image(imagem, caption="Dashboard Interativo", use_column_width=True)
-    imagem = Image.open("Imagens/dashpower.png")
-    st.image(imagem, caption="Dashboard Interativo", use_column_width=True)
->>>>>>> 71990449c58f942b05dd48591bcb7343d02a6cdf
